f97157d4cb922e63523fc6ae72d3592ef8b253d8
Jorge Lopez <jorgext@gmail.com>
2020-12-09 22:07:15 -0300

    cleanup of dash app

--- dashplotants/app.py
+++ dashplotants/app.py
@@ -36,30 +36,6 @@ app.layout = html.Center([
         [dbc.Input(id="input_path", placeholder="Path to MS", type="text")],
         style={'width': '100%', 'display': 'inline-block'}
     ),
-    # html.Br(),
-    # # test upload
-    # html.Div([
-    #     dcc.Upload(
-    #         id='upload-data',
-    #         children=html.Div([
-    #             'Drag and Drop or ',
-    #             html.A('Select Files')
-    #         ]),
-    #         style={
-    #             'width': '100%',
-    #             'height': '60px',
-    #             'lineHeight': '60px',
-    #             'borderWidth': '1px',
-    #             'borderStyle': 'dashed',
-    #             'borderRadius': '5px',
-    #             'textAlign': 'center',
-    #             'margin': '10px'
-    #         },
-    #         # Allow multiple files to be uploaded
-    #         multiple=True
-    #     ),
-    #     html.Div(id='upload-data-info'),
-    # ]),
     # Buttons
     html.Div([
         html.Br(),
